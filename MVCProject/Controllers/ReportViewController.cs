ceaeec2a669028eb781c8270a188d03dbaa4ed21
TanNguyen <itnbtan@gmail.com>
2016-07-02 00:09:18 +0700

    Fix

--- MVCProject/Controllers/ReportViewController.cs
+++ MVCProject/Controllers/ReportViewController.cs
@@ -18,18 +18,8 @@ namespace MVCProject.Controllers
         // GET: ReportView
         private retailEntities db = new retailEntities();
         private aspnetEntities _db = new aspnetEntities();
-        private Datasets.DataSetRetails dsDetail = new Datasets.DataSetRetails();
-        private InvoiceDetailRptParams InvoiceRptParams;
         private InvoiceDetailParams InvoiceParams;
-        private byte[] byte_pdf = null;
-        public ActionResult Index()
-        {
-            string path = Request.MapPath(Request.ApplicationPath) + @"Report\ReportOrderDetail.rdlc";
-            GetDetail();
-            ViewBag.ReportViewer = ShowReport.ProcessShowReport(path, "DataSetOrderDetail", dsDetail.Tables["OrdersDetail"], InvoiceRptParams.rptParams, out byte_pdf);
-            TempData["bytePDF"] = byte_pdf;
-            return View();
-        }
+        private byte[] byte_pdf = null;  
         public ActionResult ViewPDF()
         {
             return File((byte[])TempData["bytePDF"], "application/pdf");
@@ -86,46 +76,7 @@ namespace MVCProject.Controllers
             }
 
         }
-        #region function
-        List<OrdersDetail> GetDetail()
-        {
-            string code = Request.QueryString["code"];
-            if (code == null) return null;
-            var o = db.Orders.Where(c => c.OrderCode == code).FirstOrDefault();
-            var list = db.OrdersDetails.Where(c => c.OrderCode == code).ToList();
-            var us = _db.AspNetUsers.Single(c => c.Id == o.IDAccount);
-            string enu = Security.EncryptString("User:" + us.UserName + "~FrontendUser", false, EncryptType.TripleDES);
-            var u = _db.AppNetUserTypes.Find(enu);
-            var l = _db.Locations.Find(u.LocationID);
-
-            foreach (var item in list)
-            {
-                DataRow dr = dsDetail.OrdersDetail.NewRow();
-                dr["IDProduct"] = item.IDProduct;
-                dr["ProductCode"] = item.ProductCode;
-                dr["Amount"] = item.Amount;
-                dr["Price"] = item.Price;
-                dr["Tax"] = item.Tax;
-                dr["Total"] = item.Total;
-                //dr["ProductName"] = ((List<Product>)HttpContext.Application["listProduct"]).Where(a => a.ItemCode == item.ProductCode).FirstOrDefault().ProductName;
-                //dr["Unit"] = ((List<Product>)HttpContext.Application["listProduct"]).Where(a => a.ItemCode == item.ProductCode).FirstOrDefault().Unit;
-                dsDetail.OrdersDetail.Rows.Add(dr);
-            }
-            InvoiceRptParams = new InvoiceDetailRptParams();
-            InvoiceRptParams.Address = u.Address + ", Q." + u.District;
-            InvoiceRptParams.AmountInWord = Common.ConvertNumToWord.So_chu(Int64.Parse(o.Total.ToString().Replace(".00", "")));
-            InvoiceRptParams.CellPhone = u.Phone;
-            InvoiceRptParams.Contact = "_";
-            InvoiceRptParams.Discount = "_";
-            InvoiceRptParams.Name = u.DisplayName;
-            InvoiceRptParams.Total = o.TotalWithoutTax.ToString("n0");
-            InvoiceRptParams.TotalVAT = o.Total.ToString("n0");
-            InvoiceRptParams.VAT = o.Tax.ToString();
-            InvoiceRptParams.InvoiceNum = o.OrderCode;
-            InvoiceRptParams.InvoiceDate = DateTime.ParseExact(o.DateCreate, "yyyyMMddHHmmss", System.Globalization.CultureInfo.InvariantCulture).ToString("dd/MM/yyyy");
-
-            return list;
-        }
+        #region function     
         private decimal? TotalPay(string id)
         {
             var listPaymentDetail = (from l in _db.PaymentDetails where l.IDAccount == id select l).ToList();
